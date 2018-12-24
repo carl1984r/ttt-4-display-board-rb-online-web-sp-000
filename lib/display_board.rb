584b8fded95bb9d47593eea1932481ad562e077f
Bry Cox <carl.cc@gmail.com>
2018-12-24 03:05:22 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,8 @@
 
 rows = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 def display_board(rows)
-  puts "#{rows[0]}|#{rows[1]}|#{rows[2]}"
+  puts " #{rows[0]} | #{rows[1]} | #{rows[2]} "
   puts "-----------"
   puts "#{rows[3]}|#{rows[4]}|#{rows[5]}"
-  puts "-----------"
   puts "#{rows[6]}|#{rows[7]}|#{rows[8]}"
 end
